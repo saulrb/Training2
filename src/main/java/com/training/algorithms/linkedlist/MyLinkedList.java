b92ad1523a623f38c9c90cf43fb8fd9c181d54b8
saul rosales <saul.rosalesb@gmail.com>
2023-08-12 19:49:56 -0400

    Added let code questions for linked lists

--- src/main/java/com/training/algorithms/linkedlist/MyLinkedList.java
+++ src/main/java/com/training/algorithms/linkedlist/MyLinkedList.java
@@ -1,182 +1,220 @@
 package com.training.algorithms.linkedlist;
 
 public class MyLinkedList {
-    private Node head;
-    private Node tail;
-    private int length;
+  private Node head;
+  private Node tail;
+  private int length;
 
-    public MyLinkedList(int value) {
-        Node newNode = new Node(value, null);
-        head = newNode;
-        tail = newNode;
-        length = 1;
+  public MyLinkedList(int value) {
+    Node newNode = new Node(value, null);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void printList() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.getValue());
+      if (temp.getNext() != null) {
+        System.out.print(" -> ");
+      }
+      temp = temp.getNext();
     }
+    System.out.println();
+  }
 
-    public void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.getValue());
-            temp = temp.getNext();
-        }
+  public void append(int value) {
+    var node = new Node(value, null);
+    if (length == 0) {
+      head = node;
+      tail = node;
+    } else {
+      tail.setNext(node);
+      tail = node;
     }
+    length++;
+  }
 
-    public void append(int value) {
-        var node = new Node(value, null);
-        if (length == 0) {
-            head = node;
-            tail = node;
-        } else {
-            tail.setNext(node);
-            tail = node;
-        }
-        length++;
+  public void prepend(int value) {
+    Node newNode = new Node(value, null);
+    if (length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.setNext(head);
+      head = newNode;
     }
+    length++;
+  }
 
-    public void prepend(int value) {
-        Node newNode = new Node(value, null);
-        if (length == 0) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            newNode.setNext(head);
-            head = newNode;
-        }
-        length++;
+  public Node removeFirst() {
+    Node temp = null;
+    if (length == 0) {
+      return temp;
+    } else {
+      temp = head;
+      head = head.getNext();
     }
-
-    public Node removeFirst() {
-        Node temp = null;
-        if (length == 0) {
-            return temp;
-        } else {
-            temp = head;
-            head = head.getNext();
-        }
-        length--;
-        if (length == 0) {
-            tail = null;
-        }
-        return temp;
+    length--;
+    if (length == 0) {
+      tail = null;
     }
+    return temp;
+  }
 
-    public Node removeLast() {
-        if (length == 0) return null;
-        Node pre = head;
-        Node temp = head;
-        while (temp.getNext() != null) {
-            pre = temp;
-            temp = temp.getNext();
-        }
-        tail = pre;
-        tail.setNext(null);
-        length--;
-        if (length == 0) {
-            tail = null;
-            head = null;
-        }
-        return temp;
+  public Node removeLast() {
+    if (length == 0) return null;
+    Node pre = head;
+    Node temp = head;
+    while (temp.getNext() != null) {
+      pre = temp;
+      temp = temp.getNext();
     }
-
-    public boolean set(int index, int value) {
-        Node temp = get(index);
-        if (temp != null) {
-            temp.setValue(value);
-            return true;
-        }
-        return false;
+    tail = pre;
+    tail.setNext(null);
+    length--;
+    if (length == 0) {
+      tail = null;
+      head = null;
     }
+    return temp;
+  }
 
-    public boolean insert(int index, int value) {
-        if (index < 0 || index > length) {
-            return false;
-        }
-        if (index == 0) {
-            prepend(value);
-            return true;
-        }
-        if (index == length) {
-            append(value);
-            return true;
-        }
-        Node newNode = new Node(value, null);
-        Node temp = get(index - 1);
-        newNode.setNext(temp.getNext());
-        temp.setNext(newNode);
-        length++;
-        return true;
+  public boolean set(int index, int value) {
+    Node temp = get(index);
+    if (temp != null) {
+      temp.setValue(value);
+      return true;
     }
+    return false;
+  }
 
-    public Node get(int index) {
-        if (index < 0 || index >= length) {
-            return null;
-        }
-        Node temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.getNext();
-        }
-        return temp;
+  public boolean insert(int index, int value) {
+    if (index < 0 || index > length) {
+      return false;
     }
-
-    public void getHead() {
-        System.out.println("Head:" + head.getValue());
+    if (index == 0) {
+      prepend(value);
+      return true;
     }
-
-    public void getTail() {
-        System.out.println("Tail:" + tail.getValue());
+    if (index == length) {
+      append(value);
+      return true;
     }
+    Node newNode = new Node(value, null);
+    Node temp = get(index - 1);
+    newNode.setNext(temp.getNext());
+    temp.setNext(newNode);
+    length++;
+    return true;
+  }
 
-    public void getLength() {
-        System.out.println("Length:" + length);
+  public Node get(int index) {
+    if (index < 0 || index >= length) {
+      return null;
     }
-
-    public Node remove(int index) {
-        if (index < 0 || index >= length) return null;
-        if (index == 0) return removeFirst();
-        if (index == length - 1) return removeLast();
-        Node prev = get(index - 1);
-        Node temp = prev.getNext();
-        prev.setNext(temp.getNext());
-        temp.setNext(null);
-        length--;
-        return temp;
+    Node temp = head;
+    for (int i = 0; i < index; i++) {
+      temp = temp.getNext();
     }
+    return temp;
+  }
 
-    public void reverse() {
-        Node temp = head;
-        head = tail;
-        tail = temp;
-        Node after = null;
-        Node before = null;
-        for (int i = 0; i < length; i++) {
-            after = temp.getNext();
-            temp.setNext(before);
-            before = temp;
-            temp = after;
-        }
+  public void getHead() {
+    System.out.println("Head:" + head.getValue());
+  }
+
+  public void getTail() {
+    System.out.println("Tail:" + tail.getValue());
+  }
+
+  public void getLength() {
+    System.out.println("Length:" + length);
+  }
+
+  public Node remove(int index) {
+    if (index < 0 || index >= length) return null;
+    if (index == 0) return removeFirst();
+    if (index == length - 1) return removeLast();
+    Node prev = get(index - 1);
+    Node temp = prev.getNext();
+    prev.setNext(temp.getNext());
+    temp.setNext(null);
+    length--;
+    return temp;
+  }
+
+  public void reverse() {
+    Node temp = head;
+    head = tail;
+    tail = temp;
+    Node after = null;
+    Node before = null;
+    for (int i = 0; i < length; i++) {
+      after = temp.getNext();
+      temp.setNext(before);
+      before = temp;
+      temp = after;
     }
+  }
 
-
-    public Node findMiddleNode() {
-        if (length == 0) return null;
-        if (length == 1) return head;
-        if (length % 2 == 0) {
-            return get((length / 2) + 1);
-        }
-        var middleIndex = (int) Math.floor(length / 2);
-        return get(middleIndex);
+  public Node findMiddleNode() {
+    if (length == 0) return null;
+    if (length == 1) return head;
+    if (length % 2 == 0) {
+      return get((length / 2) + 1);
     }
+    var middleIndex = (int) Math.floor(length / 2);
+    return get(middleIndex);
+  }
 
-    public boolean hasLoop() {
-        if (length == 0) return false;
-        if (length == 1) return false;
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.getNext() != null) {
-            slow = slow.getNext();
-            fast = fast.getNext().getNext();
-            if (slow == fast) return true;
-        }
-        return false;
+  public boolean hasLoop() {
+    if (length == 0) return false;
+    if (length == 1) return false;
+    Node slow = head;
+    Node fast = head;
+    while (fast != null && fast.getNext() != null) {
+      slow = slow.getNext();
+      fast = fast.getNext().getNext();
+      if (slow == fast) return true;
     }
+    return false;
+  }
 
+  public Node findKthFromEnd(int i) {
+    if (length == 0) return null;
+    if (i > length || ((length - i) + 1) < 0) {
+      return null;
+    }
+    return get((length - i) + 1);
+  }
+
+  public void reverseBetween(int m, int n) {
+    if (m == n) return;
+    Node prev = null;
+    Node curr = head;
+    while (m > 1) {
+      prev = curr;
+      curr = curr.getNext();
+      m--;
+      n--;
+    }
+    Node con = prev;
+    Node tail = curr;
+    Node third;
+    while (n > 0) {
+      third = curr.getNext();
+      curr.setNext(prev);
+      prev = curr;
+      curr = third;
+      n--;
+    }
+    if (con != null) {
+      con.setNext(prev);
+    } else {
+      head = prev;
+    }
+    tail.setNext(curr);
+  }
 }
--- src/test/java/com/training/algorithms/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/training/algorithms/linkedlist/MyLinkedListTest.java
@@ -1,145 +1,173 @@
 package com.training.algorithms.linkedlist;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MyLinkedListTest {
 
-    MyLinkedList subject;
+  MyLinkedList subject;
 
-    @BeforeEach
-    void setUp() {
-        subject = new MyLinkedList(0);
-    }
+  @BeforeEach
+  void setUp() {
+    subject = new MyLinkedList(0);
+  }
 
-    @Test
-    void printAllMethods(){
-        subject.getHead();
-        subject.getTail();
-        subject.getLength();
-        subject.printList();
-        assertEquals(true,true);
-    }
-    @Test
-    void appendAndLastTest(){
-        subject.append(2);
-        subject.printList();
-        assertEquals(true,true);
-    }
-    @Test
-    void appendAndRemoveTest(){
-        subject.append(2);
-        subject.printList();
-        subject.getLength();
-        System.out.println(subject.removeLast().getValue());
-        System.out.println(subject.removeLast().getValue());
-        System.out.println(subject.removeLast());
-        assertEquals(true,true);
-    }
+  @Test
+  void printAllMethods() {
+    subject.getHead();
+    subject.getTail();
+    subject.getLength();
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void prependTest(){
-        subject.append(2);
-        subject.prepend(3);
-        subject.getLength();
-        subject.printList();
-        assertEquals(true,true);
-    }
+  @Test
+  void appendAndLastTest() {
+    subject.append(2);
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void removeFirstTest(){
-        subject.append(2);
-        System.out.println(subject.removeFirst().getValue());
-        System.out.println(subject.removeFirst().getValue());
-        System.out.println(subject.removeFirst());
-        assertEquals(true,true);
-    }
+  @Test
+  void appendAndRemoveTest() {
+    subject.append(2);
+    subject.printList();
+    subject.getLength();
+    System.out.println(subject.removeLast().getValue());
+    System.out.println(subject.removeLast().getValue());
+    System.out.println(subject.removeLast());
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testGetMethod(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        System.out.println(subject.get(2).getValue());
-        subject.printList();
-        assertEquals(true,true);
-    }
+  @Test
+  void prependTest() {
+    subject.append(2);
+    subject.prepend(3);
+    subject.getLength();
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testInsertMethod(){
-        subject.append(2);
-        subject.insert(1,1);
-        subject.printList();
-        assertEquals(true,true);
-    }
+  @Test
+  void removeFirstTest() {
+    subject.append(2);
+    System.out.println(subject.removeFirst().getValue());
+    System.out.println(subject.removeFirst().getValue());
+    System.out.println(subject.removeFirst());
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testSetMethod(){
-        subject.append(2);
-        subject.set(1,1);
-        subject.printList();
-        assertEquals(true,true);
-    }
+  @Test
+  void testGetMethod() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    System.out.println(subject.get(2).getValue());
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testReverse(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        subject.reverse();
-        subject.printList();
-        assertEquals(true,true);
-    }
+  @Test
+  void testInsertMethod() {
+    subject.append(2);
+    subject.insert(1, 1);
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testFindMiddleNode(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        subject.append(4);
-        assertEquals(2,subject.findMiddleNode().getValue());
-    }
+  @Test
+  void testSetMethod() {
+    subject.append(2);
+    subject.set(1, 1);
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testFindMiddleNodeOdd(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        subject.append(4);
-        subject.append(5);
-        assertEquals(4,subject.findMiddleNode().getValue());
-    }
+  @Test
+  void testReverse() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.reverse();
+    subject.printList();
+    assertEquals(true, true);
+  }
 
-    @Test
-    void testFindMiddleNodeEmpty(){
-        subject.removeFirst();
-        assertEquals(null,subject.findMiddleNode());
-    }
+  @Test
+  void testFindMiddleNode() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.append(4);
+    assertEquals(2, subject.findMiddleNode().getValue());
+  }
 
-    @Test
-    void testFindMiddleNodeOneOnly(){
-        assertEquals(0,subject.findMiddleNode().getValue());
-    }
+  @Test
+  void testFindMiddleNodeOdd() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.append(4);
+    subject.append(5);
+    assertEquals(4, subject.findMiddleNode().getValue());
+  }
 
-    @Test
-    void testDoesnotHasALoop(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        subject.append(4);
-        assertFalse(subject.hasLoop());
-    }
+  @Test
+  void testFindMiddleNodeEmpty() {
+    subject.removeFirst();
+    assertEquals(null, subject.findMiddleNode());
+  }
 
-    @Test
-    void testDoesHasALoop(){
-        subject.append(1);
-        subject.append(2);
-        subject.append(3);
-        subject.append(4);
-        subject.get(3).setNext(subject.get(1));
-        assertTrue(subject.hasLoop());
-    }
+  @Test
+  void testFindMiddleNodeOneOnly() {
+    assertEquals(0, subject.findMiddleNode().getValue());
+  }
+
+  @Test
+  void testDoesNotHasALoop() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.append(4);
+    assertFalse(subject.hasLoop());
+  }
+
+  @Test
+  void testDoesHasALoop() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.append(4);
+    subject.get(3).setNext(subject.get(1));
+    assertTrue(subject.hasLoop());
+  }
+
+  @Test
+  void findKthFromEnd() {
+    subject.append(1);
+    subject.append(2);
+    subject.append(3);
+    subject.append(4);
+    subject.append(5);
+    assertEquals(4, subject.findKthFromEnd(3).getValue());
+    assertEquals(2, subject.findKthFromEnd(5).getValue());
+    assertEquals(1, subject.findKthFromEnd(6).getValue());
+    assertNull(subject.findKthFromEnd(7));
+  }
+
+  @Test
+  void reverseBetween() {
+      subject.append(1);
+      subject.append(2);
+      subject.append(3);
+      subject.append(4);
+      subject.append(5);
+      subject.printList();
+      subject.reverseBetween(2, 4);
+      subject.printList();
+      assertEquals(3, subject.get(1).getValue());
+  }
 
 }
